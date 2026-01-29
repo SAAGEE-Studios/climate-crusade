cc8f874824ef8b3cfb82a793a8c03aecd474254b
ebitimidambo <ebitimidambo@icloud.com>
2026-01-28 18:00:22 -0700

    Added Front end functionality

--- client/Login/scenes/Start.js
+++ client/Login/scenes/Start.js
@@ -5,11 +5,11 @@ export class Start extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('background', 'assets/space.png');
-        this.load.image('logo', 'assets/phaser.png');
+        this.load.image('background', './client/Shared/space.png');
+        this.load.image('logo', '/Shared/phaser.png');
 
         //  The ship sprite is CC0 from https://ansimuz.itch.io - check out his other work!
-        this.load.spritesheet('ship', 'assets/spaceship.png', { frameWidth: 176, frameHeight: 96 });
+        this.load.spritesheet('ship', '/Shared/spaceship.png', { frameWidth: 176, frameHeight: 96 });
     }
 
     create() {
--- server/database/initialize.sql
+++ server/database/initialize.sql
@@ -1,11 +1,18 @@
+-- Drop tables in correct order to avoid foreign key conflicts
+DROP TABLE IF EXISTS LEVEL_PROGRESS;
+DROP TABLE IF EXISTS USERS;
+
+-- Create USERS table
 CREATE TABLE USERS (
   user_id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT NOT NULL UNIQUE,
   email TEXT NOT NULL UNIQUE,
   password_hash TEXT NOT NULL,
-  date_of_birth TEXT NOT NULL
+  date_of_birth TEXT NOT NULL,
+  first_time_play INTEGER NOT NULL DEFAULT 1
 );
 
+-- Create LEVEL_PROGRESS table
 CREATE TABLE LEVEL_PROGRESS (
   user_id INTEGER NOT NULL,
   level_id TEXT NOT NULL,
