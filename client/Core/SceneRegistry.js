d0ab4123c6425edaf365fcb42ea41348f6a7af1b
ebitimidambo <ebitimidambo@icloud.com>
2026-03-06 19:10:38 -0700

    Added Level Integration

--- client/Core/SceneRegistry.js
+++ client/Core/SceneRegistry.js
@@ -5,16 +5,19 @@ import { FirstTimeCutsceneScene } from '../Login/scenes/FirstTimeCutsceneScene.j
 import { LevelSelectScene } from '../LevelSelect/LevelSelectScene.js';
 
 import { Level01EntryScene } from '../Levels/Level01/Level01EntryScene.js';
+//import { Level01CutsceneScene } from '../Levels/Level01/Cutscene/CutsceneScene.js';
 import { AcidDownpourLevel } from '../Levels/Level01/Scenes/AcidDownpourLevel.js';
 
-import { Level02EntryScene } from '../Levels/Level02/Level01EntryScene.js';
-import { DeepPurgeLevel } from '../Levels/Level02/Scenes/DeepPurgeLevel.js';
+//import { Level02EntryScene } from '../Levels/Level02/Level01EntryScene.js';
+//import { Level02CutsceneScene } from '../Levels/Level02/Cutscene/CutsceneScene.js';
+//import { DeepPurgeLevel } from '../Levels/Level02/Scenes/DeepPurgeLevel.js';
 
-import { Level03EntryScene } from '../Levels/Level03/Level03EntryScene.js';
-import { SunkenWellsLevel } from '../Levels/Level03/Scenes/SunkenWellsLevel.js';
+//import { Level03EntryScene } from '../Levels/Level03/Level03EntryScene.js';
+//import { Level03CutsceneScene } from '../Levels/Level03/Cutscene/CutsceneScene.js';
+//import { SunkenWellsLevel } from '../Levels/Level03/Scenes/SunkenWellsLevel.js';
 
-import { Level04_EntryScene } from '../Levels/Level04/EntryScene.js';
-import { Level04_CutsceneScene } from '../Levels/Level04/Cutscene/CutsceneScene.js';
+//import { Level04EntryScene } from '../Levels/Level04/EntryScene.js';
+//import { Level04CutsceneScene } from '../Levels/Level04/Cutscene/CutsceneScene.js';
 import { SpaceJunkLevel } from '../Levels/Level04/Scenes/space_junk_level.js';
 
 export const SCENES = [
@@ -22,10 +25,21 @@ export const SCENES = [
   //LoginScene,
   //SignupScene,
   //FirstTimeCutsceneScene,
-  //LevelSelectScene,
+  LevelSelectScene,
+  //------Level 01--------
   //Level01EntryScene,
+  //Level01CutsceneScene,
   AcidDownpourLevel,
-  Level04_EntryScene,
-  Level04_CutsceneScene,
+  //------Level 02--------
+  //Level02EntryScene,
+  //Level02CutsceneScene,
+  //DeepPurgeLevel,
+  //------Level 03--------
+  //Level03EntryScene,
+  //Level03CutsceneScene,
+  //SunkenWellsLevel,
+  //------Level 04--------
+  //Level04EntryScene,
+  //Level04CutsceneScene,
   SpaceJunkLevel,
 ];
