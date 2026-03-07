98eaf68d2564f6eb0c364bb67b5f6bc53b819268
ebitimidambo <ebitimidambo@icloud.com>
2026-03-06 17:55:09 -0700

    Added more scenes to SceneRegistry

--- client/Core/SceneRegistry.js
+++ client/Core/SceneRegistry.js
@@ -7,6 +7,12 @@ import { LevelSelectScene } from '../LevelSelect/LevelSelectScene.js';
 import { Level01EntryScene } from '../Levels/Level01/Level01EntryScene.js';
 import { AcidDownpourLevel } from '../Levels/Level01/Scenes/AcidDownpourLevel.js';
 
+import { Level02EntryScene } from '../Levels/Level02/Level01EntryScene.js';
+import { DeepPurgeLevel } from '../Levels/Level02/Scenes/DeepPurgeLevel.js';
+
+import { Level03EntryScene } from '../Levels/Level03/Level03EntryScene.js';
+import { SunkenWellsLevel } from '../Levels/Level03/Scenes/SunkenWellsLevel.js';
+
 import { Level04_EntryScene } from '../Levels/Level04/EntryScene.js';
 import { Level04_CutsceneScene } from '../Levels/Level04/Cutscene/CutsceneScene.js';
 import { SpaceJunkLevel } from '../Levels/Level04/Scenes/space_junk_level.js';
