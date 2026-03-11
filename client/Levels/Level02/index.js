fb631a0fbf4e919033ce32a2de8397555d335e8a
ade123a <bashirniyi@gmail.com>
2026-03-10 23:49:16 -0600

    Updated the Game scene class

--- client/Levels/Level02/index.js
+++ client/Levels/Level02/index.js
@@ -1,13 +1,7 @@
 import { Level02EntryScene } from './Level02EntryScene.js';
 import { Level02GameScene } from './Scenes/Level02GameScene.js';
-import { Level02UIScene } from './Scenes/Level02UIScene.js';
-import { Level02WinScene } from './Scenes/Level02WinScene.js';
-import { Level02LoseScene } from './Scenes/Level02LoseScene.js';
 
 export const Level02Scenes = [
     Level02EntryScene,
     Level02GameScene,
-    Level02UIScene,
-    Level02WinScene,
-    Level02LoseScene
 ];
